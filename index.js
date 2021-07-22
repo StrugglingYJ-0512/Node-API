afa40559f1004411eafebdfb39e00eeae5b2559f
username <StrugglingYJ-0512.name>
2021-07-22 14:53:20 +0900

    nodejs의 http 모듈로 서버구축.
    1. 코드가 무자비하게 길어진다.
    2. 중복되는 코드 & 길어짐으로 가독성이 떨어진다. 이를 위해 앞으로 express FrameWork를 사용 할 것이다.

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3001;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n'); // end 함수로 client에게 hello world 보내줌.
+
+  // 사용자의 요청한 정보의 경로명에 맞게 분기한다. 
+  // req : 사용자의 요청 정보 
+  console.log(req.url);
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n'); // end 함수로 client에게 hello world 보내줌.
+  } else if (req.url === '/users') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('User list\n'); // end 함수로 client에게 hello world 보내줌.
+
+  }
+  else {
+    res.statusCode = 404;
+    res.end('Not Found')
+  }
+
+
 });
 
 // listen함수 : 서버를 요청 대기상태로 만들어줌. (서버를 종료시키지 않고 계속 대기중.)
