f9054dcdace51b6ae0513b7184f9cc763921de62
username <StrugglingYJ-0512.name>
2021-07-22 20:18:52 +0900

    Err 미들웨어

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan') // 3rdParty middleWare 모듈 
 
-// 미들웨어 생성시에는 인자가 3개.
-function logger(req, res, next) {
-  console.log('i am logger')
-  next();
+// 일반 미들웨어 (인자 : 3개)
+function commonMW(req, res, next) {
+  console.log('This is commonMW')
+  next(new Error('error ouccered')) //error 발생 ==> next 함수에 Err객체를 던져준다.
+
+}
+// Error 미들웨어.
+function errormw(err, req, res, next) { // 이 전 middleWare에서 넘겨준 err객체를 err파라미터가 받는다. 
+  console.log(err.message) // 전달받은 error를 처리 or 처리 못했으면, 다음 middleWare 로 넘김
+  next(); // 여기에서는 Error를 처리 했다고 치고, next를 넘긴다. 
 }
 
-function logger2(req, res, next) {
-  console.log('i am logger2')
-  next()
-}
-
-// 미들웨어 추가시 use()쓴다. 
-// 추가할 미들웨어를 (괄호) 안에 쓴다. 
-app.use(logger)
-app.use(logger2);
-app.use(morgan('dev'))
+app.use(commonMW);
+app.use(errormw);
 
 app.listen(3000, () => {
   console.log('Server is running')
