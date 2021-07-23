aeb1ca4762236f2e89d53834c7f079ea9ac910cf
username <StrugglingYJ-0512.name>
2021-07-23 10:25:01 +0900

    TDD- 검증모듈_3rdParty: should 사용

--- utils.spec.js
+++ utils.spec.js
@@ -2,18 +2,18 @@
 // specification(명세) ; 보통 테스트 코드가 그 테스트 대상의 명세서, 요구사항 이다
 
 const utils = require('./utils')
-const assert = require('assert') // 맞냐 안맞냐 검증하는 모듈
+const should = require('should') // 검증 3rdparty 모듈 : should
 
 // 테스트 환경을 만듬. 
 describe('utils.js모듈의 capitalize()함수는 ', () => {
   it('문자열의 첫번째 문자를 대문자로 반환한다.', () => {
-    // 두번째 파라미터 : 콜백 / 실제 테스트할 테스트 코드 작성
     const result = utils.capitalize('hello')
-    assert.equal(result, 'Hello') // 두 인자가 같은지 검증
+    result.should.be.equal('Hello');
+    // 아까보다 더 영어문장을 읽는 느낌
+    // 즉, 코드의 가독성이 높아짐. 
   })
-  // 첫번째 파라미터 : 어떤 테스트 케이스인지 설명하는 문자열을 넣어줌.
 
-}) // test 대상을 서술식으로 표현함.
+})
 
 
 
