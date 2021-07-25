5a22a0508f0f600628c3bc59474f76665c4aa7ba
username <StrugglingYJ-0512.name>
2021-07-25 16:42:56 +0900

    TDD - SuperTest 코드

--- index.js
+++ index.js
@@ -20,3 +20,5 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+// 지금까지는 객체를 할당하였으나, 변수를 할당하여도 괜찮음!
+module.exports = app;
--- utils.js
+++
@@ -1,9 +0,0 @@
-function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-  // charAt(0); 첫번째 문자. /toUpperCase(); 대문자
-}
-
-
-module.exports = {
-  capitalize: capitalize
-}
