9a93aa2d3c05a130e27cffb33a51bab2bdeaa235
username <StrugglingYJ-0512.name>
2021-07-25 17:09:23 +0900

    사용자 목록 조회 api

--- index.spec.js
+++ index.spec.js
@@ -1,19 +1,20 @@
-const app = require('./index') // 테스트 하는 파일
 const request = require('supertest') // supertest 모듈 가져옴
-
+const should = require('should')
+const app = require('./index') // 테스트 하는 파일
 
 // test 코드 작성
 // test suit : describe
 describe('GET /users는', () => {
-  //test case : it
-  it('...', (done) => { // 비동기의 해결로, 콜백함수에 파라미터로 done이라는 콜백함수를 넣어준다. 
-    // it의 첫번째 인자 : test 코드 설명
-    // 콜백 : supersTest 실행 / 실제 test 코드
-    request(app)
-      .get('/users') // get으로 요청을 실제 보냄. 
-      .end((err, res) => { // end 콜백함수로 결과 확인. 
-        console.log(res.body) // 실제 데이터 - 배열이 찍히는지. 
-        done() // done이라는  콜백함수 호출
-      })
+  describe('성공시', () => {
+    it('user 객체를 담은 배열로 응답한다.', (done) => {
+      request(app)
+        .get('/users')
+        .end((err, res) => {
+          // 배열이라는 것 검증 - should
+          res.body.should.be.instanceOf(Array)
+          done()
+        })
+    })
   })
+
 })
